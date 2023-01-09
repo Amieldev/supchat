0a34eab5984617790d52d563ffcbced093796643
Amieldev <amielsentayehu@gmail.com>
2023-01-09 19:00:22 +0300

    Added initial files and folder structures

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
-const express=require('express');
-const cors=require('cors');
-const mongoose=require('mongoose');
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
 const app=express();
 
-require('dotenv').config();
+app.use(express.static('public'));
+
+app.use(express.json({limit:"30mb"}));
+app.use(express.urlencoded({limit:"30mb"}));
