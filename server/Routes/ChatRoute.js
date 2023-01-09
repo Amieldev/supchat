1c4460426cc5f71fabaf19480a0ece96d5f84198
Amieldev <amielsentayehu@gmail.com>
2023-01-09 19:15:38 +0300

    Added ChatRoute for the express chat route

--- server/Routes/ChatRoute.js
+++ server/Routes/ChatRoute.js
@@ -3,5 +3,7 @@ import express from 'express';
 const router=express.Router();
 
 router.post('/',createChat);
+router.get('/:userId',userChats);
+router.get('/find/:firstId/:secondId',findChat);
 
 export default router
