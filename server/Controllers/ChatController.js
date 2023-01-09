84b7fe1059f4a20b5bb9fa19226155db07f5f6af
Amieldev <amielsentayehu@gmail.com>
2023-01-09 20:39:08 +0300

    Added extend option for the express middle ware

--- server/Controllers/ChatController.js
+++ server/Controllers/ChatController.js
@@ -1,4 +1,4 @@
-import ChatModel from '../Models/ChatModel';
+import ChatModel from '../Models/ChatModel.js';
 
 export const createChat=async(req,res)=>{
     const newChat=new ChatModel({
--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
-import ChatRoute from './Routes/ChatRoute';
+import ChatRoute from './Routes/ChatRoute.js';
 
 
 const app=express();
@@ -11,8 +11,8 @@ const app=express();
 //use of express middle ware
 app.use(express.static('public'));
 
-app.use(express.json({limit:"30mb"}));
-app.use(express.urlencoded({limit:"30mb"}));
+app.use(express.json({limit:"30mb",extended:true}));
+app.use(express.urlencoded({limit:"30mb",extended:true}));
 
 //use of routes
 
