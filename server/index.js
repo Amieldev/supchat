9a1f2a437c5f2873c9156c446e84f62998f70482
Amieldev <amielsentayehu@gmail.com>
2023-01-09 19:16:52 +0300

    Setup basic express structure and imported wanted items

--- server/index.js
+++ server/index.js
@@ -3,9 +3,17 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
+import ChatRoute from './Routes/ChatRoute';
+
+
 const app=express();
 
+//use of express middle ware
 app.use(express.static('public'));
 
 app.use(express.json({limit:"30mb"}));
 app.use(express.urlencoded({limit:"30mb"}));
+
+//use of routes
+
+app.use('/chat',ChatRoute)
